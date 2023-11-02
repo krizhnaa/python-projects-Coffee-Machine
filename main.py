e79dc330c823ad9f81091baeaa309c6d17df362b
krizhnaa <krizhnaprem@gmail.com>
2023-11-02 17:42:23 +0530

    Made few modifications

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cmdata
 
 cm_continue = True
 
-while cm_continue != False:
+while cm_continue != 'False':
     user_inp = input("What would you like? (espresso/latte/cappuccino): ")
 
     # print(cmdata.menu['espresso']['ingredients']['water'])
@@ -18,17 +18,15 @@ while cm_continue != False:
     if user_inp == 'off':
         cm_continue = False
 
-    def rescheck(coffee, rescheck):
-        return cmdata.menu[coffee]['ingredients'][rescheck]
+    def rescheck(coffee, material):
+        return cmdata.menu[coffee]['ingredients'][material]
 
     if user_inp == 'latte' or user_inp == 'espresso' or user_inp == 'cappuccino':
         if rescheck(user_inp, 'water') >= cmres('water'):
             print("Sorry there is shortage in Water")
-        elif rescheck(user_inp, 'milk') >= cmres('milk'):
+        elif user_inp != 'espresso' and rescheck(user_inp, 'milk') >= cmres('milk'):
             print("Sorry there is shortage in Milk")
         elif rescheck(user_inp, 'coffee') >= cmres('coffee'):
             print("Sorry there is shortage in Coffee")
 
-    # print(rescheck(user_inp, 'water'))
-    # print(cmres('water'))
 
